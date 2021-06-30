8c6041f8552b1b82f53a87ad278f438730390ab7
Matias Lago Evia <code@backyardmonk.com>
2021-06-30 13:43:19 -0400

    Add CSS normalize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import 'normalize.css';
+import './index.css';
 import { App } from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
