0a34df4831b72863ceca92548c6a90e4a7b4f6ec
Matias Lago Evia <code@backyardmonk.com>
2021-07-07 07:39:56 -0400

    Fix webpack misconfiguration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: './public/index.html',
+  template: './src/index.html',
   filename: './index.html',
-  favicon: './public/favicon.ico',
   publicPath: '/',
 });
 
