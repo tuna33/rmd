3346759843132b6a97118c4146026877240dd2cc
Matias Lago Evia <code@backyardmonk.com>
2021-07-12 18:10:06 -0400

    Refactor webpack config; Add typescript support
    Thanks to https://www.skcript.com/svr/using-webpack-with-react-typescript/

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: './src/index.html',
-  filename: './index.html',
-  publicPath: '/',
-});
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const path = require("path")
+const isProd = process.env.NODE_ENV === "production";
 
-const path = require('path');
-
-module.exports = {
-  mode: 'development',
+const config = {
+  mode: isProd ? "production" : "development",
+  entry: {
+    index: "./src/index.tsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    }
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(tsx?|jsx?)$/,
+        use: "babel-loader",
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
       },
       {
         test: /\.css$/,
@@ -28,20 +38,29 @@ module.exports = {
       },
     ],
   },
-  devServer: {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+}
+
+if(isProd) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 9000,
+    open: true,
+    hot: true,
+    compress: true,
+    stats: "errors-only",
+    overlay: true,
     historyApiFallback: true,
-  },
-  plugins: [htmlPlugin],
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@views': path.resolve(__dirname, 'src/views'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-    },
-  },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-  },
-};
+  }
+}
+
+module.exports = config;
